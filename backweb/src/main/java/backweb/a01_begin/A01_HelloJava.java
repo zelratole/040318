79cb101fb70c22577c62ad9ca7fd5d2caec2bd22
human <human@DESKTOP-UPLC68H>
2024-04-18 13:23:58 +0900

    2222

---
+++ backweb/src/main/java/backweb/a01_begin/A01_HelloJava.java
@@ -0,0 +1,18 @@
+package backweb.a01_begin;
+
+public class A01_HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+(+/-)
+		// ctrl+m
+		// 한라인 주석문
+		/* 여러라인 주석문
+		 * ctrl+s : 저장
+		 * ctrl+f11 : 실행 단축명령
+		 * */
+		System.out.println("자바시작");
+		
+	}
+
+}
