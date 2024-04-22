a40227fe1da77b97e2e982f043ce2da4f7bd6ca9
human <human@DESKTOP-UPLC68H>
2024-04-22 14:48:42 +0900

    111

--- backweb/src/main/java/backweb/a01_begin/A09_Calculator.java
+++ backweb/src/main/java/backweb/a01_begin/A09_Calculator.java
@@ -44,11 +44,42 @@ public class A09_Calculator {
 		System.out.println("청소년인 경우:"+ (age>=14 && age<18));
 		// 두가지 조건이 모두 true일 때..
 		System.out.println("무료 입장인 경우:"+(age<5 || age>=65));
-		// 5세 미만이거나 65세이상 인경(둘 중에 하나만 true인 경우)
+		// 5세 미만이거나 65세이상인 경우(둘 중에 하나만 true인 경우)
 		System.out.println("청소년이 아닌 경우:"+ !(age>=14 && age<18));
 		int pt = (int)Math.floor( Math.random()*101 );
+		// 0<=Math.random()<1
+		// Math.floor() : 내림처리 리턴 실수형
+		// (int)Math.floor( Math.random()*101 ) :0~100 임의의 정수
+		int pt2 =Integer.parseInt( ""+Math.random()*101 );
 		// 0~100점수 랜덤 처리..
 		System.out.println("점수:"+pt+", 결과:"+(pt>=70?"합격":"불합격"));
+		// ex) A팀과 B팀 주사위를 2개씩 굴려
+		//     A팀이 승인 경우 : true/false
+		//     B팀이 승인 경우 : true/false
+		//     무승부 경우 : true/false
+		int dice1 = (int)Math.floor(Math.random()*6+1);
+		int dice2 = (int)Math.floor(Math.random()*6+1);
+		int team01 = dice1 + dice2;
+		int dice3 = (int)Math.floor(Math.random()*6+1);
+		int dice4 = (int)Math.floor(Math.random()*6+1);
+		int team02 = dice3 + dice4;
+		System.out.println("A팀 : "+team01);
+		System.out.println("B팀 : "+team02);
+		System.out.println("A팀이 승인 경우 : "+(team01>team02));
+		System.out.println("B팀이 승인 경우 : "+(team01<team02));
+		System.out.println("무승부 경우 : "+(team01==team02));
+		// 중첩적 3항연산자 처리
+		// 조건1?"결과1"?(조건2?"결과2":"결과3")
+		String result = team01>team02?"A팀 승":
+			(team01<team02?"B팀 승":"무승부");
+		System.out.println("결과:"+result);
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
