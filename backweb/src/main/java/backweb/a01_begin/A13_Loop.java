a0491bf299be859ba121d06d98d48fd46d971962
human <human@DESKTOP-UPLC68H>
2024-04-23 09:46:06 +0900

    111

---
+++ backweb/src/main/java/backweb/a01_begin/A13_Loop.java
@@ -0,0 +1,63 @@
+package backweb.a01_begin;
+
+public class A13_Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+# 반복문 처리
+1. for문
+	for(초기값;반복조건;증감연산자){
+		반복할 내용
+	}
+2. while문
+	while(반복조건){
+		반복할 내용..
+	}
+3. do{
+      1반이상 반복할 내용(일단 최소한 한번은 반복)
+   }while(반복조건);
+4. continue/break;
+   continue : 해당 반복 step 해당 단계만 처리하지 않고
+    다음 반복 step 진행
+   break : 반복을 중단처리하는 구문
+5. 반복문 안에 반복문
+	구구단..
+
+6. 반복문과 조건문
+	반복을 중단 또는 여러가지 알고리즘 처리시 사용..
+7. 반복문과 배열
+	1)배열 선언
+		String [] arry = {"사과","바나나","딸기"};
+		for(int idx=0;idx<arry.length;idx++){
+			arry[idx] 
+		}
+
+   
+ * */
+		// 
+		System.out.println("# 자바의 기본 for문 #");
+		for(int count=1;count<=10;count++) {
+			System.out.println("카운트 :"+count);
+		}	
+		// ex1) 5~15까지 출력
+		System.out.println("# 5~15 출력 #");
+		for(int cnt = 5;cnt<=15;cnt++) {
+			System.out.print(cnt+", ");
+		}
+		System.out.println();
+		// ex2) 100~90까지 출력
+		System.out.println("# 100~90 출력 #");
+		for(int cnt= 100;cnt>=90;cnt--) {
+			System.out.print(cnt+", ");
+		}
+		System.out.println();
+		// ex3) 1~20까지 홀수만 출력(증감연산자 cnt+=2  활용)
+		System.out.println("# 1~20 출력 홀수 #");
+		for(int cnt=1;cnt<=20;cnt+=2) {
+			System.out.print(cnt+", ");
+		}
+		System.out.println();
+	}
+
+}
