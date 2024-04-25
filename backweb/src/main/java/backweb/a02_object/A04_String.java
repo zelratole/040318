450494eeeaf2204ca9d7fc0496ac46d3eb1b306c
human <human@DESKTOP-UPLC68H>
2024-04-25 14:19:36 +0900

    111

---
+++ backweb/src/main/java/backweb/a02_object/A04_String.java
@@ -0,0 +1,37 @@
+package backweb.a02_object;
+
+public class A04_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+# String 문자열 객체
+1. char : 데이터 하나를 할당하는 문자형 데이터 유형.
+2. char[] : 위 문자가 모여서 문자배열이 되어 객체로 사용한다.
+3. char[]를 문자 배열이 주로 문자를 이루는 단어를 만들어서 주로 활용되어
+	String이라는 문자열 객체를 만들어 활용하게 되었다.
+4. String은  문자배열에서 파생했기에 문자가 가지고 있는 여러가지 형식을 차용하고
+	접근 할 수 있다.
+	.chatAt(index위치) : 문자열을 구성하고 있는 요소들 문자 하나씩 index로
+		가져온다.
+	.substring(시작, 마지막) : String을 구성하고 있는 문자의 시작과 마지막 
+		위치의 문자를 가져온다.
+5. String의 메모리 활용.
+	1) String은 객체이기에 heap영역의 데이터를 사용한다.
+	2) String 선언 형식에 따라 heap영역의 메모리를 다르게 사용한다.
+		String str1 = new String("홍길동");
+		String str2 = new String("홍길동");
+		// 두 문자열은 동일한 문자열이지만 str1, str2라고 참조값이 다르기에
+		 다른 문자위치의 heap영역을 참조 한다.
+		 그래서 str1 == str2는 false가 처리된다.
+		 (자바만의 특징)
+		  
+		
+		
+		
+		
+		
+ * */
+	}
+
+}
