ec21b7eff00462e40ff14f00fd6fce71f5ee3823
human <human@DESKTOP-UPLC68H>
2024-05-01 11:01:52 +0900

    1111

---
+++ backweb/src/main/java/backweb/a02_object/a02_relation/vo/Engine.java
@@ -0,0 +1,25 @@
+package backweb.a02_object.a02_relation.vo;
+
+public class Engine {
+	private int hoursepower;
+
+	public Engine() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Engine(int hoursepower) {
+		super();
+		this.hoursepower = hoursepower;
+	}
+
+	public int getHoursepower() {
+		return hoursepower;
+	}
+
+	public void setHoursepower(int hoursepower) {
+		this.hoursepower = hoursepower;
+	}
+	
+	
+}
