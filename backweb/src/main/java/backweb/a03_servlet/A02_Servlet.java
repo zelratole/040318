7fe8ed9d8b225659959b93564009e90a0c8df4d2
human <human@DESKTOP-UPLC68H>
2024-05-02 16:11:31 +0900

    111

---
+++ backweb/src/main/java/backweb/a03_servlet/A02_Servlet.java
@@ -0,0 +1,43 @@
+package backweb.a03_servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class A02_Servlet
+ */
+public class A02_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public A02_Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out =  response.getWriter();
+		String name="higirl";
+		out.print("<h1>"+name+"! Hello! servlet promgramming 2!!</h1>");	
+		
+		
+		// ctrl+s 저장 서버 리스타트..
+		
+		// A03_Servlet.java   call03.do 
+		// 출력내용 :  Hello  no3  
+		// 호출 해서 처리되는 것 확인..`
+		
+		
+	}
+
+}
