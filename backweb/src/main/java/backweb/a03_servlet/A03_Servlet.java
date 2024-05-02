cbcd58a243fdaeafcdc5e0033a1e6534410ad664
human <human@DESKTOP-UPLC68H>
2024-05-02 16:20:42 +0900

    1111

---
+++ backweb/src/main/java/backweb/a03_servlet/A03_Servlet.java
@@ -0,0 +1,36 @@
+package backweb.a03_servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class A03_Servlet
+ */
+public class A03_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public A03_Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out =  response.getWriter();
+		String name="good Man!";
+		out.print("<h1>"+name+"! Hello! servlet promgramming 3!!</h1>");	
+		
+	}
+
+}
