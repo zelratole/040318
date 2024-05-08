ac9b704b70a42ee1b7708954223fda6fa5c57d1b
human <human@DESKTOP-UPLC68H>
2024-05-09 08:54:56 +0900

    11111

---
+++ backweb/src/main/java/backweb/z02_exam/A0502.java
@@ -0,0 +1,17 @@
+package backweb.z02_exam;
+
+public class A0502 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyMember p01 = new MyMember();
+		MyMember p02 = new MyMember();
+		System.out.println(p01 == p02);
+		System.out.println(p01.equals(p02));
+		
+	}
+
+}
+class MyMember{
+	
+}
