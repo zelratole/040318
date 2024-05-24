1746268bb1845a3b67fd42354db87dce06ea38b4
human <human@DESKTOP-UPLC68H>
2024-05-24 10:08:58 +0900

    1111

---
+++ backweb/src/main/java/backweb/a04_database/DbConn.java
@@ -0,0 +1,39 @@
+package backweb.a04_database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConn {
+	public static Connection con() throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe");
+	}
+	public static void close(AutoCloseable... resources) { 
+		for(AutoCloseable resource:resources) {
+			if(resource!=null) {
+				try {
+					resource.close();
+				} catch (Exception e) {
+					System.out.print(resource.getClass().getSimpleName());
+					System.out.println("닫기 실패");
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			close( con() );
+			System.out.println("접속성공");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
