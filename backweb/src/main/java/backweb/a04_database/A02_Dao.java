ef7ee855caabb1c509a8e57c6b43cfe8fe67dcb2
human <human@DESKTOP-UPLC68H>
2024-05-24 16:10:56 +0900

    111

--- backweb/src/main/java/backweb/a04_database/A02_Dao.java
+++ backweb/src/main/java/backweb/a04_database/A02_Dao.java
@@ -42,34 +42,37 @@ public class A02_Dao {
 		return ob;
 	}
 	// 조회하는 template : 복사해서 핵심부분만 변경경해서 사용
-	public Object tempCRUD(String ins) {
+	public int tempCRUD(String ename, String job) {
 		// 1. 조회하여 결과를 리턴할 객체를 선언한다.(select문에 의한 결과값을 리턴할 내용)
-		Object ob = new Object();
+		int cudCnt  = 0;
 		// 2. 사용되는 sql 구문을 처리한다.
-		String sql = "SELECT * FROM emp01 WHERE ename like ? ";
+		String sql = "INSERT INTO emp01(ename, job) VALUES(?,?) ";
 		// 3. 본격적으로 연결/대화/결과/자원해제예외처리..
 		//    매개변수로 처리할 때 자원해제 처리된다.
 		try( Connection con = DBConn.con();
 			 PreparedStatement pstmt = con.prepareStatement(sql); ){
-			 pstmt.setString(1, "%"+ins+"%");
-			try(ResultSet rs = pstmt.executeQuery();){
-				//System.out.println("데이터 있음:"+rs.next());
-				//System.out.println("첫번째 행의 첫번째 열데이터:"+rs.getString(1));
-				while(rs.next()) {
-					System.out.print(rs.getInt("empno")+"\t");
-					System.out.print(rs.getString("ename")+"\t");
-					System.out.print(rs.getDouble("sal")+"\t");
-					System.out.print(rs.getInt("deptno")+"\n");
-				}
-			}
+			 con.setAutoCommit(false); // auto commit 방지
+			 pstmt.setString(1, ename);
+			 pstmt.setString(2, job);
+			 cudCnt = pstmt.executeUpdate();
+			 if(cudCnt>0) {
+				 System.out.println(cudCnt+"건 등록 성공!");
+				 con.commit();
+			 }else {
+				 System.out.println("등록 안 됨");
+				 con.rollback();
+			 }
 			
 		}catch(SQLException e) {
 			System.out.println("DB 처리 에러:"+e.getMessage());
+//			if(con!=null) {
+//				
+//			}
 		}catch(Exception e) {
 			System.out.println("일반 에러:"+e.getMessage());
 		}
 		
-		return ob;
+		return cudCnt;
 	}
 	public Dept getDept(int deptno) {
 		Dept dept = null;
@@ -168,6 +171,9 @@ public class A02_Dao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		A02_Dao dao = new A02_Dao();
+		System.out.println("등록?"+dao.tempCRUD("마길동","사원"));
+		
+		
 		for(Emp01 emp:dao.getEmpList("", "")) {
 			System.out.print(emp.getEmpno()+"\t");
 			System.out.print(emp.getEname()+"\t");
