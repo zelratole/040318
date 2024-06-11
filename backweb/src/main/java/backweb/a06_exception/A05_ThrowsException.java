1d913f5438f7b648fc0bc016d6a7618774045dc7
human <human@DESKTOP-UPLC68H>
2024-06-11 14:05:56 +0900

    1111

--- backweb/src/main/java/backweb/a06_exception/A05_ThrowsException.java
+++ backweb/src/main/java/backweb/a06_exception/A05_ThrowsException.java
@@ -1,10 +1,27 @@
 package backweb.a06_exception;
 
-public class A05_ThrowsException {
+import java.sql.SQLException;
 
+public class A05_ThrowsException {
+	
+	void method1() throws SQLException{ // 예외 위임
+		throw new SQLException("SQLException 발생");
+	}
+	void method2() throws NullPointerException{ // 예외 위임
+		throw new NullPointerException("NullPointerException 발생");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		A05_ThrowsException e = new A05_ThrowsException();
+		try {
+			e.method2();
+			e.method1();
+			
+		} catch (SQLException e1) {  // 위임된 예외 처리
+			System.out.println(e1.getMessage());
+		} catch(NullPointerException e2) {   // 위임된 예외 처리
+			System.out.println(e2.getMessage());
+		}
 	}
 /*
 # 예외 위임
@@ -35,3 +52,7 @@ public class A05_ThrowsException {
 
  * */
 }
+
+
+
+
