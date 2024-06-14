4dfa00edf8052a9b41a4d8b38b213ceca7f98661
human <human@DESKTOP-UPLC68H>
2024-06-14 15:45:43 +0900

    111

---
+++ springweb/src/main/java/a01_diexp/z01_vo/Cpu.java
@@ -0,0 +1,28 @@
+package a01_diexp.z01_vo;
+
+public class Cpu {
+	private String model;
+	private String company;
+	public Cpu() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Cpu(String model, String company) {
+		super();
+		this.model = model;
+		this.company = company;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+}
