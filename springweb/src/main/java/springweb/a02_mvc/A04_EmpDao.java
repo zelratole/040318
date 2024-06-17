574e56abeccd8250bd52f6efceca83eeafc8ad55
human <human@DESKTOP-UPLC68H>
2024-06-17 09:14:28 +0900

    11111

--- springweb/src/main/java/springweb/a02_mvc/A04_EmpDao.java
+++ springweb/src/main/java/springweb/a02_mvc/A04_EmpDao.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
-// springweb.a02_mvc.A04_EmpDao
+
 @Component
 public class A04_EmpDao implements A03_EmpDaoInf {
 
@@ -51,6 +51,152 @@ public class A04_EmpDao implements A03_EmpDaoInf {
 		}
 	
 		return empList;
+	}
+
+	public int insertEmp(Emp ins) {
+		int insCnt = 0;
+		String sql = "INSERT INTO emp05\r\n" + "	values(?,?,?,?,to_date(?,'YYYY-MM-DD'),?,?,?)";
+		Connection con2 = null;
+		try (   Connection con = DBconJ.getConnection(); // 웹서버에 로딩 후, 화면 실행시
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
+			con2 = con;
+			
+			con.setAutoCommit(false); // auto commit 방지
+			pstmt.setInt(1, ins.getEmpno());
+			pstmt.setString(2, ins.getEname());
+			pstmt.setString(3, ins.getJob());
+			pstmt.setInt(4, ins.getMgr());
+			pstmt.setString(5, ins.getHiredateStr());
+			pstmt.setDouble(6, ins.getSal());
+			pstmt.setDouble(7, ins.getComm());
+			pstmt.setInt(8, ins.getDeptno());
+			insCnt = pstmt.executeUpdate();
+			if (insCnt > 0) {
+				System.out.println(insCnt + "건 등록 성공!");
+				con.commit();
+			}
+		} catch (SQLException e) {
+			System.out.println("DB 처리 에러:" + e.getMessage());
+			if (con2 != null) {
+				try {
+					con2.rollback();
+				} catch (SQLException e1) {
+					System.out.println("롤백 예외");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("일반 에러:" + e.getMessage());
+		}
+	
+		return insCnt;
+	}
+
+	public Emp getEmp(int empno) {
+		Emp emp = null;
+		String sql = "SELECT * \r\n" + "	  FROM emp05\r\n" + "   WHERE empno = ?";
+		try (  //Connection con = DBConn.con(); // main() 에서 테스트용
+			  Connection con = DBconJ.getConnection(); // 웹서버에 로딩 후, 화면 실행시
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
+			pstmt.setInt(1, empno);
+			try (ResultSet rs = pstmt.executeQuery();) {
+				if (rs.next()) {
+					emp = new Emp(rs.getInt("empno"), rs.getString("ename"), rs.getString("job"), rs.getInt("mgr"),
+							rs.getDate("hiredate"), rs.getDouble("sal"), rs.getDouble("comm"), rs.getInt("deptno"));
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("DB 처리 에러:" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("일반 에러:" + e.getMessage());
+		}
+		return emp;
+	}
+
+	public int updateEmp(Emp upt) {
+		int uptCnt = 0;
+		String sql = "  update emp05\r\n"
+				+ "			set ename=?,\r\n"
+				+ "			    job=?,\r\n"
+				+ "			    mgr=?,\r\n"
+				+ "			    hiredate=to_date(?,'YYYY-MM-DD'),\r\n"
+				+ "			    sal=?,\r\n"
+				+ "			    comm=?,\r\n"
+				+ "			    deptno=?\r\n"
+				+ "		  where empno=?";
+		
+		Connection con2 = null;
+		try (//Connection con = DBConn.con(); // main() 에서 테스트용
+				 Connection con = DBconJ.getConnection(); // 웹서버에 로딩 후, 화면 실행시
+			 PreparedStatement pstmt = con.prepareStatement(sql);) {
+			con2 = con;
+			con.setAutoCommit(false); // auto commit 방지
+			pstmt.setString(1, upt.getEname());
+			pstmt.setString(2, upt.getJob());
+			pstmt.setInt(3, upt.getMgr());
+			pstmt.setString(4, upt.getHiredateStr());
+			pstmt.setDouble(5, upt.getSal());
+			pstmt.setDouble(6, upt.getComm());
+			pstmt.setInt(7, upt.getDeptno());
+			pstmt.setInt(8, upt.getEmpno());
+			uptCnt = pstmt.executeUpdate();
+			if (uptCnt > 0) {
+				System.out.println(uptCnt + "건 등록 성공!");
+				con.commit();
+			} else {
+				System.out.println("등록 안 됨");
+				con.rollback();
+			}
+	
+		} catch (SQLException e) {
+			System.out.println("DB 처리 에러:" + e.getMessage());
+			if (con2 != null) {
+				try {
+					con2.rollback();
+				} catch (SQLException e1) {
+					System.out.println("롤백 예외");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("일반 에러:" + e.getMessage());
+		}
+	
+		return uptCnt;
+	}
+
+	public int deleteEmp(int empno) {
+		int uptCnt = 0;
+		String sql = "  delete from  emp05\r\n"
+				+ "		  where empno=?";	
+		Connection con2 = null;
+		try (//Connection con = DBConn.con(); // main() 에서 테스트용
+				 Connection con = DBconJ.getConnection(); // 웹서버에 로딩 후, 화면 실행시
+			 PreparedStatement pstmt = con.prepareStatement(sql);) {
+			con2 = con;
+			con.setAutoCommit(false); // auto commit 방지
+			pstmt.setInt(1, empno);
+			uptCnt = pstmt.executeUpdate();
+			if (uptCnt > 0) {
+				System.out.println(uptCnt + "건 삭제 성공!");
+				con.commit();
+			} else {
+				System.out.println("삭제 안 됨");
+				con.rollback();
+			}
+	
+		} catch (SQLException e) {
+			System.out.println("DB 처리 에러:" + e.getMessage());
+			if (con2 != null) {
+				try {
+					con2.rollback();
+				} catch (SQLException e1) {
+					System.out.println("롤백 예외");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("일반 에러:" + e.getMessage());
+		}
+	
+		return uptCnt;
 	}
 
 }
