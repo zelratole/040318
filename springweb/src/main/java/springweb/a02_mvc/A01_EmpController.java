9c05c8dba857947a3713177620251b07eacdd0ac
human <human@DESKTOP-UPLC68H>
2024-06-17 09:42:24 +0900

    1111

--- springweb/src/main/java/springweb/a02_mvc/A01_EmpController.java
+++ springweb/src/main/java/springweb/a02_mvc/A01_EmpController.java
@@ -1,10 +1,14 @@
 package springweb.a02_mvc;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 //springweb.a02_mvc.A01_EmpController
 //springweb.a02_mvc.A04_EmpDao
@@ -27,6 +31,28 @@ public class A01_EmpController {
 		// view 화면호출..
 		return "WEB-INF\\views\\a02_mvc\\a01_empList.jsp";
 	}
-	
+	// get방식으로 초기 화면 호출..
+	// http://localhost:7080/springweb/empListAjaxFrm.do
+	@RequestMapping("empListAjaxFrm.do")
+	public String empListAjaxFrm() {
+		return "WEB-INF\\views\\a03_ajax\\a01_empList.jsp";
+	}
+	// http://localhost:7080/springweb/empListData.do
+	@RequestMapping("empListData.do")
+	@ResponseBody
+	public String empListData(Emp sch, Model d) {
+		Gson g = new GsonBuilder().setPrettyPrinting().create();
+		return g.toJson(service.getEmpList(sch));
+	}	
+	// http://localhost:7080/springweb/empListData2.do
+	@RequestMapping("empListData2.do")
+	public String empListData2(Emp sch, Model d) {
 
+		d.addAttribute("empList", service.getEmpList(sch));
+		System.out.println(service.getEmpList(sch).size());
+		// view 화면호출..
+		return "jsonView";
+	}
+	
+	
 }
