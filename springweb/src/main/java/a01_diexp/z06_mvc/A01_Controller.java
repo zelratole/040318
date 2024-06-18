59eb9c5e2eb795822d7990cb6d03c75ab269cd91
human <human@DESKTOP-UPLC68H>
2024-06-18 12:45:03 +0900

    1111

--- springweb/src/main/java/a01_diexp/z06_mvc/A01_Controller.java
+++ springweb/src/main/java/a01_diexp/z06_mvc/A01_Controller.java
@@ -1,8 +1,22 @@
 package a01_diexp.z06_mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class A01_Controller {
-
+	@Autowired
+	private A02_Service service;
+	
+	// 요청처리 : url + 요청key/val
+	@RequestMapping("/empList.do")
+	public String  getEmpList(String sch, Model d) { // String sch : 요청값 처리
+		d.addAttribute("empList", service.getList(sch)); // Model d를 통해 모델 데이터 처리..
+		System.out.println("서비스단 호출:"+service.getList(sch));
+		return "jsp View 호출/json view 호출";//view단호출..
+	}
+	
+	
 }
--- springweb/src/main/java/a01_diexp/z06_mvc/A02_Service.java
+++ springweb/src/main/java/a01_diexp/z06_mvc/A02_Service.java
@@ -1,8 +1,18 @@
 package a01_diexp.z06_mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class A02_Service {
 
+	@Autowired(required = false)  // 해당 객체가 containter에 없더라도 nullpointer 에러 발생 방지 해줌..
+	private A03_Dao dao;  
+	// ? 인터페이스임에도 불구하고 아래와 메서드에서 객체 생성 메서드 호출 가능 이유??
+	// 인터페이스(추상) = new 실제객체();
+	// 참조.메서드() ==> 다형성에 의해서 실제객체가 가지고 있는 메서드 활용..
+	
+	public String getList(String sch) {
+		return dao.getList(sch);
+	}
 }
