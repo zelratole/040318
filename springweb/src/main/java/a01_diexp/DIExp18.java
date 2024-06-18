a33f7ef26d31a0902d74e3fea51abcd63db22e01
human <human@DESKTOP-UPLC68H>
2024-06-18 10:10:06 +0900

    1111

--- springweb/src/main/java/a01_diexp/DIExp18.java
+++ springweb/src/main/java/a01_diexp/DIExp18.java
@@ -3,6 +3,9 @@ package a01_diexp;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import a01_diexp.z03_vo.Computer;
+import a01_diexp.z03_vo.Cpu;
+
 public class DIExp18 {
 
 	public static void main(String[] args) {
@@ -10,8 +13,15 @@ public class DIExp18 {
 		String path = "a01_diexp\\di18.xml"; 
 		// <bean id="obj" class="java.lang.Object"/>
 		AbstractApplicationContext ctx = new GenericXmlApplicationContext(path);
-		Object ob = ctx.getBean("obj",Object.class);
-		System.out.println("컨테이너 안에 객체:"+ob);
+		Cpu cpu = ctx.getBean("cpu",Cpu.class);
+		Computer computer = ctx.getBean("computer",Computer.class);
+		System.out.println("컨테이너 안에 객체:"+cpu);
+		System.out.println("컨테이너 안에 객체:"+computer);
+		cpu.setCompany("인텔");
+		cpu.setModel("i7 3.4GHz");
+		computer.setCompany("조립식");
+		computer.setCpu(cpu);
+		computer.useCpu();
 		ctx.close();
 	}
 
