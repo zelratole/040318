b71b40523e2cd38af93a8d83d6672dd97146ab57
human <human@DESKTOP-UPLC68H>
2024-06-19 11:23:54 +0900

    111

--- springweb/src/main/java/springweb/a03_mvc/a03_dao/A01_Dao.java
+++ springweb/src/main/java/springweb/a03_mvc/a03_dao/A01_Dao.java
@@ -2,6 +2,7 @@ package springweb.a03_mvc.a03_dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import springweb.z01_vo.Dept;
@@ -13,6 +14,12 @@ public interface A01_Dao {
 			+ "WHERE ename like '%'||#{ename}||'%' " // sch.getEname()
 			+ "and job like '%'||#{job}||'%' ")      //  sch.getJob()
 	List<Emp> getEmpList(Emp sch);
+	
+	@Insert("INSERT INTO emp05 values(#{empno}, #{ename},#{jog},#{mgr}, \r\n"
+			+ "	to_date(#{hiredateStr},'YYYY-MM-DD'), #{sal},#{comm},#{deptno})")
+	int insertEmp(Emp ins);
+	
+	
 	// 부서정보...
 	@Select("SELECT * FROM dept01 "
 			+ "WHERE dname like #{dname} "
