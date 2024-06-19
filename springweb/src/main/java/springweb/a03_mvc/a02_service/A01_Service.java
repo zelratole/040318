21a1a04383dc0ffd2d5ed4875c9b694243c9e508
human <human@DESKTOP-UPLC68H>
2024-06-19 09:18:43 +0900

    11111

--- springweb/src/main/java/springweb/a03_mvc/a02_service/A01_Service.java
+++ springweb/src/main/java/springweb/a03_mvc/a02_service/A01_Service.java
@@ -11,11 +11,17 @@ import springweb.z01_vo.Emp;
 
 @Service
 public class A01_Service {
+	
+	// CRUD(create read update delete) 등록 조회 수정 삭제..
+	
+	// 컨테이너에 있는 객체는 자동을 할당 처리 
+	// required=false는 혹시, 이 객체가 없더라도 객체가 없을 때, 종속된 메서드 호출시 
+	// 발생하는 nullpointerexception 방지 처리..
 	@Autowired(required=false)
 	private A01_Dao dao;
 	
 	public List<Emp> getEmpList(Emp sch){
-		
+		// 요청값이 없는 초기 화면에서 전체 검색 가능하게 처리..
 		if(sch.getEname()==null) sch.setEname("");
 		if(sch.getJob()==null) sch.setJob("");
 		
@@ -24,6 +30,8 @@ public class A01_Service {
 	public List<Dept> getDeptList(Dept sch){
 		if(sch.getDname()==null) sch.setDname("");
 		if(sch.getLoc()==null) sch.setLoc("");
+		
+		//  like ? ==> like '%요청값%'  키워드 검색이 되게..
 		sch.setDname("%"+sch.getDname()+"%");
 		sch.setLoc("%"+sch.getLoc()+"%");
 		return dao.getDeptList(sch);
--- springweb/src/main/java/springweb/a03_mvc/a03_dao/A01_Dao.java
+++ springweb/src/main/java/springweb/a03_mvc/a03_dao/A01_Dao.java
@@ -9,15 +9,16 @@ import springweb.z01_vo.Emp;
 
 public interface A01_Dao {
 	
-	@Select("SELECT * FROM EMP "
+	@Select("SELECT * FROM EMP05 "
 			+ "WHERE ename like '%'||#{ename}||'%' " // sch.getEname()
 			+ "and job like '%'||#{job}||'%' ")      //  sch.getJob()
 	List<Emp> getEmpList(Emp sch);
 	// 부서정보...
-	@Select("SELECT * FROM dept "
+	@Select("SELECT * FROM dept01 "
 			+ "WHERE dname like #{dname} "
 			+ "AND loc like #{loc} ")
 	List<Dept> getDeptList(Dept sch);
+	// @Insert @Update @Delete
 	
 	
 	
