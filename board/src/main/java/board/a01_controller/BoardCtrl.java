65d3b5c6e7fd229163330f78e1b46c453c968582
human <human@DESKTOP-UPLC68H>
2024-06-24 15:35:41 +0900

    1111

---
+++ board/src/main/java/board/a01_controller/BoardCtrl.java
@@ -0,0 +1,14 @@
+package board.a01_controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardCtrl {
+	// http://localhost:7080/board/boardList.do
+	@RequestMapping("boardList.do")
+	public String boardList(Model d) {
+		return "a01_boardList";
+	}
+}
