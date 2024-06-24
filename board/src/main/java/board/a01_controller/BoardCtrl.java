759b747ef7f0c6ed2d7e1c02a0e0a65cbf330cc2
human <human@DESKTOP-UPLC68H>
2024-06-24 15:37:17 +0900

    111

--- board/src/main/java/board/a01_controller/BoardCtrl.java
+++ board/src/main/java/board/a01_controller/BoardCtrl.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BoardCtrl {
 	// http://localhost:7080/board/boardList.do
+	
 	@RequestMapping("boardList.do")
 	public String boardList(Model d) {
 		return "a01_boardList";
