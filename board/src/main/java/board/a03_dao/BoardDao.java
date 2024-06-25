a6db77b9edc560b17e81e51a3dfb743f1d30cfa7
human <human@DESKTOP-UPLC68H>
2024-06-25 15:05:22 +0900

    111

--- board/src/main/java/board/a03_dao/BoardDao.java
+++ board/src/main/java/board/a03_dao/BoardDao.java
@@ -2,9 +2,11 @@ package board.a03_dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import board.vo.Board;
+import board.vo.Boardfile;
 
 public interface BoardDao {
 	@Select("select * \r\n"
@@ -12,4 +14,12 @@ public interface BoardDao {
 			+ "where subject like '%'||#{subject}||'%'\r\n"
 			+ "and writer like '%'||#{writer}||'%'")
 	List<Board> getBoardList(Board sch);
+	
+	@Insert("insert into board values(board_seq.nextval,#{refno},#{subject},\r\n"
+			+ "	#{content},#{writer},#{readcnt},sysdate,sysdate) ")
+	int boardInsert(Board ins);
+	@Insert("INSERT INTO boardfile values(board_seq.currval,#{fname},\r\n"
+			+ "	#{etc},sysdate,sysdate)")
+	int boardFileInsert(Boardfile ins);	
+	
 }
