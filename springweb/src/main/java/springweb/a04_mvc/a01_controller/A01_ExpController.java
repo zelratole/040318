de48e8723fbcde71001c92e63156122fc7d94ae1
human <human@DESKTOP-UPLC68H>
2024-06-27 10:11:32 +0900

    1111

--- springweb/src/main/java/springweb/a04_mvc/a01_controller/A01_ExpController.java
+++ springweb/src/main/java/springweb/a04_mvc/a01_controller/A01_ExpController.java
@@ -2,6 +2,7 @@ package springweb.a04_mvc.a01_controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import springweb.a04_mvc.a02_service.A01_ExpService;
@@ -18,5 +19,13 @@ public class A01_ExpController {
 		service.daoExp01();
 		return "";
 	}
-	
+	// http://localhost:7080/springweb/mvc_exp02.do
+	@GetMapping("mvc_exp02.do")
+	public String mvc_exp01(Model d) {
+		// 모델데이터를 통해서 서버스에서 받아온 데이터 단일데이터 뷰단 전달.
+		d.addAttribute("job", service.getJob());
+		// 모델데이터를 통해서 서비스에서 받아온 다중 데이터를 뷰단에 전달
+		d.addAttribute("empnos", service.getEmpnos());   
+		return "WEB-INF\\views\\a05_mvcexp\\a01_view.jsp";
+	}	
 }
