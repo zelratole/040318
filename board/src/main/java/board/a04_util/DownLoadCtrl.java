33ead6ba453ae21b1dca2c42ddb1970cf05bbf9d
human <human@DESKTOP-UPLC68H>
2024-07-02 09:55:40 +0900

    111

--- board/src/main/java/board/a04_util/DownLoadCtrl.java
+++ board/src/main/java/board/a04_util/DownLoadCtrl.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 */
 @Controller
 public class DownLoadCtrl {
-	// http://localhost:7080/springweb/downLoad.do?fname=a01_spring.txt  
+	// http://localhost:7080/bord/downLoad.do?fname=a01_spring.txt  
 	// 호출시 특정 파일을 다운로드 처리..
 	@GetMapping("downLoad.do")
 	public String downLoad(@RequestParam("fname") String fname, 
