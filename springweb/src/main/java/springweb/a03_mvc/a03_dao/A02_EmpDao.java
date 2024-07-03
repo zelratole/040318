69646e87bb943a82d63378cf3a4cd9a18a688956
human <human@DESKTOP-UPLC68H>
2024-07-03 14:02:35 +0900

    111

---
+++ springweb/src/main/java/springweb/a03_mvc/a03_dao/A02_EmpDao.java
@@ -0,0 +1,12 @@
+package springweb.a03_mvc.a03_dao;
+
+import java.util.List;
+import springweb.z01_vo.Emp;
+// springweb.z01_vo.Emp
+// springweb.a03_mvc.a03_dao.A02_EmpDao
+// List<Emp> getEmpList(Emp sch)
+public interface A02_EmpDao {
+
+	List<Emp> getEmpList(Emp sch);
+	
+}
