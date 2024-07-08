72a754da232be0ae152bdbc3359c8b202315515a
human <human@DESKTOP-UPLC68H>
2024-07-08 10:35:40 +0900

    111

--- springweb/src/main/java/springweb/a02_mvc/A05_AjaxController.java
+++ springweb/src/main/java/springweb/a02_mvc/A05_AjaxController.java
@@ -3,6 +3,7 @@ package springweb.a02_mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import springweb.z01_vo.Person;
 
@@ -22,5 +23,24 @@ public class A05_AjaxController {
 		// json view에 의해 json형식 데이터 처리 
 		// {"data":{"name":"홍길동","age":25,"loc":"수원"}}
 	}
+	// http://localhost:7080/springweb/data02.do
+	@GetMapping("data02.do")
+	public String data02(@RequestParam("name") String name, Model d) {
+		d.addAttribute("person", new Person(name,25,"수원"));
+		return "jsonView";
+		// json view에 의해 json형식 데이터 처리 
+		// {"person":{"name":"홍길동","age":25,"loc":"수원"}}
+	}	
+	@GetMapping("data03.do")
+	public String data03(Person p01, Model d) {
+		p01.setAge(27);
+		p01.setLoc("서울");
+		return "jsonView";
+		// Person p01 : 모델 어트리뷰트 개념으로 요청값 처리 + 모델 데이터 처리. Person ==> person
+		// json view에 의해 json형식 데이터 처리 
+		// {"person":{"name":"홍길동","age":25,"loc":"수원"}}
+	}	
+	
+	
 	
 }
