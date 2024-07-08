ea0d3a435b5ca0d5691a42fa06665a2d573fc7bb
human <human@DESKTOP-UPLC68H>
2024-07-08 12:07:31 +0900

    111

--- springweb/src/main/java/springweb/a03_mvc/a03_dao/A04_CalendarDao.java
+++ springweb/src/main/java/springweb/a03_mvc/a03_dao/A04_CalendarDao.java
@@ -3,6 +3,7 @@ package springweb.a03_mvc.a03_dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import springweb.z01_vo.Calendar;
@@ -12,4 +13,11 @@ public interface A04_CalendarDao {
 			+ "			textcolor, allday, url\r\n"
 			+ "	from calendar")
 	List<Calendar> getFullCalendarList();
+	
+	@Insert("	INSERT INTO calendar values(cal_seq.nextval,\r\n"
+			+ "		#{title},#{writer},#{start},#{end},\r\n"
+			+ "		#{content},#{backgroundColor},#{textColor}#{allDay},#{url})")
+	int insertCalendar(Calendar ins);
+	
+	
 }
