0ed2a00d0821b96cb2d4c002bc0d720640641af0
human <human@DESKTOP-UPLC68H>
2024-07-18 11:23:06 +0900

    111

--- springweb/src/main/java/springweb/a03_mvc/a01_controller/A07_MailController.java
+++ springweb/src/main/java/springweb/a03_mvc/a01_controller/A07_MailController.java
@@ -1,14 +1,21 @@
 package springweb.a03_mvc.a01_controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import springweb.a03_mvc.a02_service.A07_MailService;
 import springweb.z01_vo.MailVo;
 
 @Controller
 public class A07_MailController {
+	
+	@Autowired(required=false)
+	private A07_MailService service;
+	
+	
 	// http://localhost:7080/springweb/mail.do
 	@GetMapping("mail.do")
 	public String mailForm() {
@@ -17,7 +24,7 @@ public class A07_MailController {
 	@PostMapping("mail.do")
 	public String sendMail(MailVo mail, Model d) {
 		
-		d.addAttribute("msg", "메일발송성공");
+		d.addAttribute("msg", service.sendMail(mail));
 		return "WEB-INF\\views\\a02_mvc\\a03_mailForm.jsp";
 	}
 	
